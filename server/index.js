e2d9a0f72b309993b923d549184f096dc2ac2ffe
Billal <billaldev99@gmail.com>
2026-07-27 13:35:04 +0100

    MAJ

--- server/index.js
+++ server/index.js
@@ -48,7 +48,9 @@ app.use(cors({
   credentials: true,
 }));
 
-app.use(helmet());
+app.use(helmet({
+  crossOriginResourcePolicy: { policy: "cross-origin" },
+}));
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ limit: '10mb', extended: true }));
 
